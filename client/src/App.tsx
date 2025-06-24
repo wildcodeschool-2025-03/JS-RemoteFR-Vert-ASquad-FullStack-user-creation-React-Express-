1e289e672f6d74f9d3d5e77d69cd17752786f5f8
IDRISSI O. Ayoub <idrissio.ayoub@gmail.com>
2025-06-24 16:34:44 +0200

    feat: react-hook-form on App.tsx + react-toastify

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,50 @@
+import { useForm } from "react-hook-form";
+import { Bounce, toast, ToastContainer } from "react-toastify";
 import "./App.css";
 
 function App() {
+  // const handleFormSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(e.target);
+  //   const firstName = e.target[0].value;
+  //   const lastName = e.target[1].value;
+
+  //   if(firstName && firstName.length > 0 && firstName.length < 50 )
+  // };
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const handleForm = (data) => {
+    delete data.confirm_password;
+
+    toast.success("Bravo c'est tout bête !");
+  };
+
+  console.log(errors);
+
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        transition={Bounce}
+      />
       <div className="flex flex-col justify-center sm:h-screen p-4">
         <div className="max-w-md w-full mx-auto border border-gray-300 rounded-2xl p-8">
-          <form>
+          <form onSubmit={handleSubmit(handleForm)}>
             <div className="space-y-6">
               <div>
                 <label
@@ -15,12 +54,30 @@ function App() {
                   Firstname
                 </label>
                 <input
+                  {...register("firstname", {
+                    required: {
+                      value: true,
+                      message: "Merci de remplir ce champ",
+                    },
+                    minLength: {
+                      value: 4,
+                      message: "Le champ doit contenir au minimum 4 caractères",
+                    },
+                    maxLength: {
+                      value: 50,
+                      message:
+                        "Le champ doit contenir au maximum 50 caractères",
+                    },
+                  })}
                   name="firstname"
                   type="text"
                   className="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
                   placeholder="John"
                 />
               </div>
+              {errors?.firstname && (
+                <span className="text-red-500">{errors.firstname.message}</span>
+              )}
               <div>
                 <label
                   htmlFor="lastname"
@@ -29,39 +86,96 @@ function App() {
                   Lastname
                 </label>
                 <input
+                  {...register("lastname", {
+                    required: {
+                      value: true,
+                      message: "Merci de remplir ce champ",
+                    },
+                    minLength: {
+                      value: 4,
+                      message: "Le champ doit contenir au minimum 4 caractères",
+                    },
+                    maxLength: {
+                      value: 50,
+                      message:
+                        "Le champ doit contenir au maximum 50 caractères",
+                    },
+                  })}
                   name="lastname"
                   type="text"
                   className="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
                   placeholder="Doe"
+                  required
+                  minLength={4}
                 />
+                {errors?.lastname && (
+                  <span className="text-red-500">
+                    {errors.lastname.message}
+                  </span>
+                )}
               </div>
               <div>
                 <label className="text-slate-900 text-sm font-medium mb-2 block">
                   Email
                 </label>
                 <input
+                  {...register("email", {
+                    required: true,
+                    pattern: {
+                      value:
+                        /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/,
+                      message: "le format d'email n'est pas celui attendu.",
+                    },
+                  })}
                   name="email"
                   type="text"
                   className="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
                   placeholder="john@doe.com"
                 />
+                {errors?.email && (
+                  <span className="text-red-500">{errors.email.message}</span>
+                )}
               </div>
               <div>
                 <label className="text-slate-900 text-sm font-medium mb-2 block">
                   Password
                 </label>
                 <input
+                  {...register("password")}
                   name="password"
                   type="password"
                   className="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
                   placeholder="Enter password"
                 />
               </div>
+              <div>
+                <label className="text-slate-900 text-sm font-medium mb-2 block">
+                  Confirm Password
+                </label>
+                <input
+                  {...register("confirm_password", {
+                    validate: (value) => {
+                      if (value !== watch("password")) {
+                        return "Les mots de passe ne correspondent pas";
+                      }
+                    },
+                  })}
+                  name="confirm_password"
+                  type="password"
+                  className="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
+                  placeholder="Confirm password"
+                />
+              </div>
+              {errors?.confirm_password && (
+                <span className="text-red-500">
+                  {errors.confirm_password.message}
+                </span>
+              )}
             </div>
 
             <div className="mt-12">
               <button
-                type="button"
+                type="submit"
                 className="w-full py-3 px-4 text-sm tracking-wider font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none cursor-pointer"
               >
                 Create an account
